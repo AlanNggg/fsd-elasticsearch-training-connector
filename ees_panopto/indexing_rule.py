63e2b025e562d6533d5c4d3ff159b31eaf8cd0ee
AlanNggg <ngpuilam9166@gmail.com>
2024-02-02 12:08:36 +0800

    update

--- ees_panopto/indexing_rule.py
+++ ees_panopto/indexing_rule.py
@@ -23,30 +23,32 @@ class IndexingRules:
         should_include = False
         # should_exclude = False
         if self.include:
-            include_ocr = {'ocr_path_template': self.include['ocr_path_template']}
+            include_ocr = {
+                'ocr_path_template': self.include['ocr_path_template']}
             # should_index_by_default: True = do ocr if no pattern, False = DO NOT DO ocr if no pattern
-            should_include, ocr_language = self.should_include_or_exclude_ocr(include_ocr, {}, file_details, 'include')
+            should_include, ocr_language = self.should_include_or_exclude_ocr(
+                include_ocr, {}, file_details, 'include')
         # if self.exclude:
         #     # should_index_by_default: True = do ocr if no pattern, False = DO NOT DO ocr if no pattern
         #     exclude_ocr = {'ocr_path_template': self.exclude['ocr_path_template']}
         #     should_exclude = self.should_include_or_exclude_ocr(exclude_ocr, include_ocr, file_details, 'exclude')
-        
-        print('should_ocr', should_include, file_details['file_path'])
+
         return should_include, ocr_language
-    
+
     def should_include_or_exclude_ocr(self, pattern_dict, is_present_in_include, file_details, pattern_type, should_ocr_by_default=False):
         should_ocr = should_ocr_by_default
         for filtertype, pattern in pattern_dict.items():
             for value in (pattern or []):
                 if is_present_in_include and (value['path'] in (is_present_in_include.get(filtertype) or [])):
                     pattern.remove(value)
-            should_ocr, ocr_language = self.follows_ocr_rule(pattern, file_details, pattern_type)
+            should_ocr, ocr_language = self.follows_ocr_rule(
+                pattern, file_details, pattern_type)
             if should_ocr is False:
                 should_ocr = False
             elif should_ocr is True:
                 return should_ocr, ocr_language
         return should_ocr, None
-    
+
     def follows_ocr_rule(self, pattern, file_details, pattern_type):
         """Applies filters on the file and returns True or False based on whether
            it follows the pattern or not
@@ -57,7 +59,7 @@ class IndexingRules:
         if pattern:
             for value in pattern:
                 path = value['path']
-                lang = value.get('language') # optional
+                lang = value.get('language')  # optional
 
                 # Define the pattern using regular expression
                 pattern = r'sectionID=(\d+)'
@@ -66,17 +68,19 @@ class IndexingRules:
                 match = re.search(path, pattern)
                 if not match:
                     return False, None
-                
+
                 section_id = match.group(1)
                 path_pattern = f'*sectionID={section_id}*'
 
                 match = re.search(path, pattern)
                 if match:
                     attachment_id = match.group(1)
-                    path_pattern = path_pattern + f"attachmentID={attachment_id}*"
-                
-                result = glob.globmatch(file_details['file_path'], path_pattern, flags=glob.GLOBSTAR)
-                if (pattern_type == 'include' and result) or (pattern_type == 'exclude' and not(result)):
+                    path_pattern = path_pattern + \
+                        f"attachmentID={attachment_id}*"
+
+                result = glob.globmatch(
+                    file_details['file_path'], path_pattern, flags=glob.GLOBSTAR)
+                if (pattern_type == 'include' and result) or (pattern_type == 'exclude' and not (result)):
                     return True, lang
 
         return False, None
